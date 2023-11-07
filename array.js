e296914ed9e8a85ca28b5673b34bd3b3f39093e5
Akash <113762183+21002624@users.noreply.github.com>
2023-11-07 11:23:38 +0530

    Update array.js

--- array.js
+++ array.js
@@ -5,7 +5,7 @@ names.forEach(name => {
     console.log(name);
 });
 
-// Example using map to add text to the end of each array element
+
 const departments = ['IT Department', 'ECE Department', 'HR Department'];
 
 const modifiedDepartments = departments.map(department => {
@@ -15,14 +15,12 @@ const modifiedDepartments = departments.map(department => {
 console.log(modifiedDepartments);
 
 
-// Example using filter to find people older than 18
 const ages = [20, 16, 25, 14, 30];
 
 const adults = ages.filter(age => age > 18);
 
 console.log(adults);
 
-// Example using reduce to find the sum and average of numbers
 const numbers = [10, 5, 8, 15, 7];
 
 const sumAndAverage = numbers.reduce((accumulator, currentValue, index, array) => {
@@ -37,7 +35,6 @@ console.log('Sum:', sumAndAverage.sum);
 console.log('Average:', sumAndAverage.avg);
 
 
-// Example using every to check if all ages are greater than 18
 const allAdults = ages.every(age => age > 18);
 
 console.log('Are all adults?', allAdults);
